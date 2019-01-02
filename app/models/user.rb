86ec66931a4c0bde2b83a290217c35b521dcaf01
John Master <john.master@ga.co>
2019-01-02 09:45:34 -0500

    Update User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :email, presence: true
+
+  def to_token_payload
+      {
+          id: id,
+          email: email
+      }
+  end
 end
